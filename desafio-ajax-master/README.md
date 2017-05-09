b68135b007310ba761a06828c56babbd4852256d
Felipe Agustoni <felipe.agustoni@castmeta.local>
2017-05-09 15:46:41 -0300

    leo

---
+++ desafio-ajax-master/README.md
@@ -0,0 +1,27 @@
+# Desafio AJAX
+
+Utilizar AJAX para popular as combos _Estado_ e _Município_ dinamicamente:
+- Regras:
+- - Pode utilizar jQuery.
+- - A combo _Município_ deve ser carregada dinamicamente ou seja ao selecionar o _Estado_ o sistema deve carregar a combo _Município_
+- - Ao selecionar o item "Selecione" da combo _Estado_, deve limpar a combo _Município_.
+- - Utilizar a estrutura desse projeto.
+- - Utilizar as URL's abaixo para popular as combos:
+  
+
+| Combo | URL |
+| ------ | ------ |
+| Estado |  http://ibge.herokuapp.com/estado/Nome
+| Cidade | http://ibge.herokuapp.com/municipio/?val={codigoCidade}
+
+# Bonus
+
+  - Apresentar tela de _Loading_ ao carregar as combos.
+  - Versionar no github.com
+
+# Prêmio
+  - Conhecimento.
+  - Talvez ser contratado quando o período de estagio terminar.
+
+
+[![N|Solid](https://i.ytimg.com/vi/sQofwsWVyAE/hqdefault.jpg)]
---
+++ desafio-ajax-master/js/index.js
@@ -0,0 +1,42 @@
+$(function(){ // Quando a pagina carrega
+
+    $.ajax({
+		type: "GET",
+		dataType: 'JSON',
+		url: "http://ibge.herokuapp.com/estado/Nome",
+		success: function(retorno){
+		 
+			$.each(retorno, function (key, val) {                
+               $('#selEstado').append('<option value="' + val + '">' + key + '</option>');
+            });
+		}
+	});
+
+$('#selEstado').change(function(){
+	$('#selMunicipio').empty();
+	$('#selMunicipio').append('<option value="">Selecione</option>');
+		var estado = $('#selEstado').val(); //Pegando o id do estado
+		//console.log(estado);
+		$.ajax({
+			type: "GET",
+			dataType: "JSON",
+			url: "http://ibge.herokuapp.com/municipio/?val=" + estado,
+			success: function(retorno){
+				//console.log(retorno);
+				$.each(retorno, function(key, val){
+					$('#selMunicipio').append('<option value="' + val + '">' + key + '</option');
+				});
+			}
+		});
+	});
+
+	$(document).on({
+	  	ajaxStart: function () {
+			$(".modal").show();
+		},
+	  	ajaxStop: function() {
+	    	$(".modal").hide();
+	  	}
+	});
+
+});
